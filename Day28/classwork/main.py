2b559b8a11c12124886c0233c5770e26f2ea803a
IrakliGeradze <ikachogeradze2007@gmail.com>
2025-11-30 12:43:40 -0800

    twenty eight commit

---
+++ Day28/classwork/main.py
@@ -0,0 +1,17 @@
+#funqcia xelaxla gamoyenebadi kodis bloki
+
+def greeting(first, last):
+    print("Hello", + first + "" + last)
+
+greeting("Irakli", "Geradze")
+
+
+#funqciebs vyoft 2 kategoriad
+#1. davalebas ro asrulebs
+#2. gamotvlian shedegs
+
+
+def greet():
+    return "Hello World"
+
+print(greet())
---
+++ Day28/homework/task1.py
@@ -0,0 +1,1 @@
+#davalebebi gvaq code wars shi.
