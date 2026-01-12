dbae0c5817ac04aeb3b5e3ecd5809cbad31edafd
IrakliGeradze <ikachogeradze2007@gmail.com>
2026-01-11 17:14:07 -0800

    forty five

---
+++ Day45 Css/classwork/script.js
@@ -0,0 +1,39 @@
+// console.log("Hello JavaScript")
+
+// let username = "Irakli";
+// let myNumber = "9";
+
+// console.log(username + myNumber)
+
+// monacemta tipis shecvlis funqciebi
+// String, parseInt,
+// console.log(typeof String(myNumber))
+
+// alert("AOPKAPOFKAWPMAP")
+// let x = 5;
+// let y = 7;
+// console.log(x + y)
+// console.dir(document.body.children[0].textContent = "U Clicked")
+//
+// ცვლადის შექმნა შედგება 2 ეტაპისგან
+// 1. დეფინიცია - განსაზღვრა
+// ცვლადის შესაქმნელად ჯავასქრიპტში ვიყენებთ 3 რესერვირებულ სიტყვას: let, const, var. java script - ში ინდენტანცია არ არსებობს
+// const x = 5; // - amas vegar shecvli
+// x = 2
+//
+// let x = 5;
+// x = 2
+// console.log(x) amas shevcvlit
+//
+//2. ინიციალიზაცია - მნიშვნელობის მინიჭება
+// x = .. aq tu ar sheinaxe mnishvneloba gamoitans Undefineds
+// 3. var - ით შექმნილი ცვლადი არის გლობალური (დღესდღეისობით აღარ იყენებენ var - ს)  
+
+// let x = 0;
+// console.dir(document.body.children[0])
+
+let x = 0;
+let myButton = document.body.children[0]
+myButton.onclick.backgroundColor = "green"
+
+// function colorChange()
